7749e406c54f0ce5071db8e3ad7bdd22b5290f44
Wangye Zhao <ivanzhaowy@gmail.com>
2013-08-13 21:37:03 +0800

    Update test case with mocha

--- test/test.js
+++ test/test.js
@@ -1,52 +1,70 @@
-module('loginAsync');
-asyncTest('loginAsync', 5, function() {
-    var Account = window.SnapPea.Account;
+describe('Account', function () {
+    describe('Account.loginAsync()', function () {
+        var Account = window.SnapPea.Account;
 
-    var userInfo = {
-    };
-    var userInfo2 = {
-        username : 'a'
-    };
-    var userInfo3 = {
-        password : 'b'
-    };
-    var userInfo4 = {
-        username : 'a',
-        password : 'b'
-    };
-    var userInfo5 = {
-        username : 'test@gmail.com',
-        password : 'testgmail'
-    };
+        var userInfo = {
+        };
+        var userInfo2 = {
+            username : 'a'
+        };
+        var userInfo3 = {
+            password : 'b'
+        };
+        var userInfo4 = {
+            username : 'a',
+            password : 'b'
+        };
+        var userInfo5 = {
+            username : 'test@gmail.com',
+            password : 'testgmail'
+        };
 
-    Account.loginAsync(userInfo).done(function () {
-        ok(false);
-    }).fail(function (resp) {
-        ok(resp.error === -2, 'Missing pramas.');
-    });
+        it('Should faild when misssing params. ', function (done) {
+            Account.loginAsync(userInfo).done(function () {
+                done('Should faild when misssing params. ');
+            }).fail(function (resp) {
+                if (resp.error === -2) {
+                    done();
+                }
+            });
+        });
 
-    Account.loginAsync(userInfo2).done(function () {
-        ok(false);
-    }).fail(function (resp) {
-        ok(resp.error === -2, 'Missing username. ');
-    });
+        it('Should faild when misssing password. ', function (done) {
+            Account.loginAsync(userInfo2).done(function () {
+                done('Should faild when misssing password. ');
+            }).fail(function (resp) {
+                if (resp.error === -2) {
+                    done();
+                }
+            });
+        });
 
-    Account.loginAsync(userInfo3).done(function () {
-        ok(false);
-    }).fail(function (resp) {
-        ok(resp.error === -2, 'Missing password. ');
-    });
+        it('Should faild when misssing username. ', function (done) {
+            Account.loginAsync(userInfo3).done(function () {
+                done('Should faild when misssing username. ');
+            }).fail(function (resp) {
+                if (resp.error === -2) {
+                    done();
+                }
+            });
+        });
 
-    Account.loginAsync(userInfo4).done(function () {
-        ok(false);
-    }).fail(function (resp) {
-        ok(resp.error === 1010, 'Wrong username or password. ');
-    });
+        it('Should faild when username or password is incorrect. ', function (done) {
+            Account.loginAsync(userInfo4).done(function () {
+                done('Should faild when username or password is wrong. ');
+            }).fail(function (resp) {
+                if (resp.error === 1010) {
+                    done();
+                }
+            });
+        });
 
-    Account.loginAsync(userInfo5).done(function () {
-        ok(true, 'Success login. ');
-        start();
-    }).fail(function (resp) {
-        ok(false);
+        it('Should pass when username and password are correct. ', function (done) {
+            Account.loginAsync(userInfo5).done(function () {
+                done();
+            }).fail(function (resp) {
+                done('Should faild when username or password are correct. ');
+            });
+        });
     });
 });
