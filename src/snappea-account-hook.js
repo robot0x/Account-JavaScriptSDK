a9f23e1dd168de82b59ab0cb52b43f6bbaf41338
NetPuter <NetPuter@Gmail.com>
2013-09-16 17:08:15 +0800

    Use `attr()` instead of `css()` to set CSS

--- src/snappea-account-hook.js
+++ src/snappea-account-hook.js
@@ -2,36 +2,35 @@
 (function (global) {
     var $ = global.$;
 
-    var IFRAME_STYLE = {
-        'border' : 'none',
-        'border-radius' : '10px',
-        'left' : '50%',
-        'height' : '450px',
-        'margin' : '-225px 0 0 -215px',
-        'position' : 'absolute',
-        'top' : '50%',
-        'width' : '430px'
-    };
+    var IFRAME_STYLE = [
+        'border: none;',
+        'border-radius: 10px;',
+        'left: 50%;',
+        'height: 450px;',
+        'margin: -225px 0 0 -215px;',
+        'position: absolute;',
+        'top: 50%;',
+        'width: 430px;'
+    ].join('');
 
-    var CTN_STYLE = {
-        'background-color' : 'rgba(0, 0, 0, .7)',
-        'bottom' : '0',
-        'left' : '0',
-        'position' : 'absolute',
-        'right' : '0',
-        'top' : '0'
-    };
+    var CTN_STYLE = [
+        'background-color: rgba(0, 0, 0, .7);',
+        'bottom: 0;',
+        'left: 0;',
+        'position: absolute;',
+        'right: 0;',
+        'top: 0;'
+    ].join('');
 
     var AccountHook = {};
 
     AccountHook.open = function (name, callback, context) {
-        var $iframe = $('<iframe>').css(IFRAME_STYLE).attr({
-            src : 'http://www.wandoujia.com/?source=web&close=1#' + name
-        });
+        var $ctn = $('<div>').attr('style', CTN_STYLE).appendTo('body');
 
-        var $ctn = $('<div>').css(CTN_STYLE).append($iframe);
-
-        $('body').append($ctn);
+        var $iframe = $('<iframe>').attr({
+            src : 'http://www.wandoujia.com/account/?source=web&close=1#' + name,
+            style : IFRAME_STYLE
+        }).appendTo($ctn);
 
         var messenger = global.Messenger.initInParent($iframe[0]);
 
