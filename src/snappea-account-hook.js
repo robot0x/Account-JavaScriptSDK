9cf3ee5904055a68b3d12397b7d30c33791c43ca
NetPuter <NetPuter@Gmail.com>
2013-09-20 18:16:26 +0800

    Add z-index to $ctn

--- src/snappea-account-hook.js
+++ src/snappea-account-hook.js
@@ -23,7 +23,8 @@
         'position: absolute;',
         'position: fixed;',
         'right: 0;',
-        'top: 0;'
+        'top: 0;',
+        'z-index: 999;'
     ].join('');
 
     var AccountHook = {};
