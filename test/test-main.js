a50a46cbbff66b8dfb6d761acc0213de8396f860
NetPuter <NetPuter@Gmail.com>
2014-02-23 18:39:05 +0800

    Only use `Zepto` in test case

--- test/test-main.js
+++ test/test-main.js
@@ -1,20 +1,11 @@
 require.config({
     baseUrl: '/base/',
     paths : {
-        jQuery : 'app/components/jquery/dist/jquery.min',
         Zepto : 'app/javascripts/zepto'
-    },
-    shim : {
-        jQuery : {
-            exports : '$'
-        },
-        Zepto : {
-            exports : 'Zepto'
-        }
     }
 });
 
-require(['jQuery'], function ($) {
+require(['Zepto'], function ($) {
     require(['test/specs/spec-sdk'], function () {
         window.__karma__.start();
     });
