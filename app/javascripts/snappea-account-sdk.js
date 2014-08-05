0baa42797c1623c8521e5a5c05b6ce390444228d
NetPuter <NetPuter@Gmail.com>
2014-08-05 10:55:02 +0800

    chore: Simplify regex of phone number

--- app/javascripts/snappea-account-sdk.js
+++ app/javascripts/snappea-account-sdk.js
@@ -746,7 +746,7 @@
     };
 
     Account.isPhoneNumber = function (input) {
-        var PHONE_PATTERN = /^(1(([3548][\d])))\d{8}$/;
+        var PHONE_PATTERN = /^1[3458]\d{9}$/;
         return PHONE_PATTERN.test(input);
     };
 
